8a450c4f6fa8f0a7585160ffb7c3dabecbb4938e
Akhmetzyanov Amir <akhmetzyanovamir2006@gmail.com>
2026-01-22 10:45:23 +0300

    trying to connect database and make reg and auth

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -28,10 +28,14 @@ func MainPage(c echo.Context) error{
 		"Title": "Main",
 	})
 }
-
 func AuthPage(c echo.Context) error{
 	return c.Render(http.StatusOK, "auth.html", map[string]interface{}{
 		"Title": "Authorization",
+	})
+}
+func RegPage(c echo.Context) error{
+	return c.Render(http.StatusOK, "reg.html", map[string]interface{}{
+		"Title": "Registration",
 	})
 }
 func ConnectingSQL() (*pgx.Conn, error) {
@@ -46,27 +50,67 @@ func ConnectingSQL() (*pgx.Conn, error) {
 	if err != nil{
 		log.Printf("Не могу подключиться к базе данных: %v", err)
 	}
+	defer conn.Close(context.Background())
+
+	_, err = conn.Exec(context.Background(), `CREATE TABLE IF NOT EXISTS users(
+		ID SERIAL PRIMARY KEY,
+		user_id INTEGER,
+		user_login VARCHAR(50),
+		user_password VARCHAR(50),
+		user_email VARCHAR(100)),
+		created_at TIMESTAMP DEFAULT NOW()`)
+	if err != nil{
+		log.Printf("Can't create table: %v\n", err)
+	}
 	return conn, err
 }
 
 func WriteDataSQL(login, password, email string) {
-	ConnectingSQL()
-	// getRegUsername := c.FormValue("login")
-	// getRegPassword := c.FormValue("password")
-
-}	
-func Authorization(c echo.Context){
+	conn, err := ConnectingSQL()
+	if err != nil {
+		log.Printf("Database connection error: %v\n", err)
+	}
+	type User struct{
+		ID int
+		Login string
+		Password string
+		Email string
+	}
+	var user User
+	err = conn.QueryRow(context.Background(), 
+		`INSERT INTO users(user_id, user_login, user_password, user_email) 
+		VALUES($1, $2, $3, $4)`).Scan(&user.ID, &user.Login, &user.Password, &user.Email)
+	if err != nil{
+		log.Printf("Can't insert data in table: %v\n", err)
+	}
+}		
+func Authorization(c echo.Context) error{
 	conn, err := ConnectingSQL()
 	if err != nil{
 		log.Printf("Database connection error: %v", err)
 	}
 	getAuthLogin := c.FormValue("auth_login")
-	getAuthPassword := c.FormValue("password")
+	getAuthPassword := c.FormValue("auth_password")
 	
-	getDatabaseLogin = conn.QueryRow(context.Background(), "")
-	getDatabasePassword = conn.QueryRow(context.Background(), "") 
+	// getDBLogin := conn.QueryRow(context.Background(), "SELECT user_login FROM users")
+	// getDBPassword := conn.QueryRow(context.Background(), "") 
 
+	rows := conn.QueryRow(context.Background(), "SELECT user_login, user_password FROM users")
+	type User struct{
+		ID int
+		Login string
+		Password string
+		Email string
+	}
+	var user User
+	err = conn.QueryRow(context.Background(), 
+		`SELECT users(user_id, user_login, user_password, user_email) 
+		VALUES($1, $2, $3, $4)`).Scan(&user.ID, &user.Login, &user.Password, &user.Email)
+	if err != nil{
+		log.Printf("Can't insert data in table: %v\n", err)
+	}
 	
+	return c.Redirect(http.StatusOK, "/reg")
 }
 
 func Handlers(){
@@ -78,7 +122,9 @@ func Handlers(){
 	e.GET("/", func(c echo.Context) error{
 		return c.Render(http.StatusOK, "auth.html", nil)
 	})
-	e.GET("/auth", AuthPage)
+	public := e.Group("/public")
+	public.GET("/auth", AuthPage)
+	public.GET("/reg", RegPage)
 
 	users := e.Group("/users")
 	users.GET("/main", MainPage)
